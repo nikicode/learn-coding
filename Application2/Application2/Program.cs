37dfde5baea78d2364bc785cb05f49e7d65d1624
NN <niki.d.nikolaev@gmail.com>
2016-11-08 15:07:17 +0200

    Added new application 2 for printing of 3 numbers.

---
+++ Application2/Application2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PrintNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(1);
+            Console.WriteLine(11);
+            Console.WriteLine(1001);
+        }
+    }
+}
